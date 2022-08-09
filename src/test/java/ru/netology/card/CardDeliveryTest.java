b6ecd0924b03b18a5581068c306be0dcc6c9fe22
Анна <marinaanna542@gmail.com>
2022-08-09 17:25:17 +0300

    fix

--- src/test/java/ru/netology/card/CardDeliveryTest.java
+++ src/test/java/ru/netology/card/CardDeliveryTest.java
@@ -10,30 +10,34 @@ import java.time.Duration;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 
 public class CardDeliveryTest {
 
-    public String generateDate() {
-        return LocalDate.now().plusDays(5).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+    public String generateDate(int shift) {
+        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
 
     }
 
 
     @Test
     void shouldCallBack() {
+        String planningDate = generateDate(5);
         open("http://localhost:9999");
         $(By.cssSelector("[data-test-id='city'] input")).setValue("Йошкар-Ола");
         $(By.cssSelector("[data-test-id='date'] input")).click();
         $(By.cssSelector("[data-test-id='date'] input")).sendKeys(Keys.CONTROL, "a", Keys.BACK_SPACE);
-        $(By.cssSelector("[data-test-id='date'] input")).setValue(generateDate());
+        $(By.cssSelector("[data-test-id='date'] input")).setValue(planningDate);
         $(By.cssSelector("[data-test-id='name'] input")).setValue("Мамин-Сибиряк Валентин");
         $(By.cssSelector("[data-test-id='phone'] input")).setValue("+77777777777");
         $(By.cssSelector("[data-test-id='agreement']")).click();
         $x("//*[text()='Забронировать']").click();
         $x("//*[contains(text(),'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
+        $(By.cssSelector("[data-test-id='notification']")).shouldHave(text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(visible);
 
     }
 
